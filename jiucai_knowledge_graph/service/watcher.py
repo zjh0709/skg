20378271ea02cfcee136465bac605c06e187b27a
u389291928 <389291928@tianchi.com>
2018-06-20 11:23:30 +0800

    fix

--- jiucai_knowledge_graph/service/watcher.py
+++ jiucai_knowledge_graph/service/watcher.py
@@ -1,16 +1,13 @@
 from jiucai_knowledge_graph.service import settings
+from daemon import DaemonContext
 import logging
 import time
 
 
 class JobWatcher(object):
-    def __init__(self):
-        self.stdin_path = settings.get("stdin_path")
-        self.stdout_path = settings.get("stdout_path")
-        self.stderr_path = settings.get("stderr_path")
-        self.pidfile_path = settings.get("pidfile_path")
-        self.pidfile_timeout = settings.get("pidfile_timeout")
-
     def run(self):
-        logging.info("now is {}".format(time.ctime()))
+        with DaemonContext():
+            f = open(settings.get("stdout_path"), "w")
+            f.write("{} \n".format(time.ctime()))
+            time.sleep(1)
 
--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from jiucai_knowledge_graph.service.watcher import JobWatcher
-from daemon.runner import DaemonRunner
 
 
 if __name__ == '__main__':
-    daemon_runner = DaemonRunner(JobWatcher())
-    daemon_runner.do_action()
+    job_watcher = JobWatcher
+    job_watcher.run()
 
