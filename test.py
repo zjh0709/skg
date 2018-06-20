ab13ec8124c876cef775e4690defa18721839cfb
u389291928 <389291928@tianchi.com>
2018-06-20 13:42:10 +0800

    fix

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from jiucai_knowledge_graph.service.watcher import JobWatcher
 from daemon import DaemonContext
+import time
 
 
 if __name__ == '__main__':
     with DaemonContext():
         job_watcher = JobWatcher()
         job_watcher.run()
+        while True:
+            time.sleep(1)
 
